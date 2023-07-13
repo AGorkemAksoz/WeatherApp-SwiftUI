0e7aa34b0b8b4ec1a60123b366fd7e2d0e92b6aa
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2023-07-13 09:53:18 +0300

    Initial Commit

---
+++ WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Gorkem on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
